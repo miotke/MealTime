34780dbf4332707797085a0e4f1243ab876d7adf
Andrew Miotke <andrewmiotke@gmail.com>
2019-12-09 16:26:28 -0800

    Removing playground

--- MyPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class APIManager {
-    let urlString = "http://127.0.0.1:8000/"
-    static let sharedInstance = APIManager()
-    static let getPostsEndpoint = "/posts/"
-}
-
-
